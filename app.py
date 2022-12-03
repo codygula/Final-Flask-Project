fe771b8995512347b539cc15780b968038394782
Cody Gula <cgula7@gmail.com>
2022-12-03 14:33:09 -0800

    mostly working

--- app.py
+++ app.py
@@ -8,26 +8,20 @@ searchdatabase = dbclient.Table("searchItems")
 
 app = Flask(__name__)
 
-@app.route('/', methods =["GET", "POST"])
-def index():
-
-    resp = scratchpad.query_table('searchItems', 'email', '222222')
-    posts = resp.get('Items')
-    print(posts)
-
-    return render_template('index.html', posts=posts)
-
-
 
 @app.route('/', methods =["GET", "POST"])
 def UserInput():
+    resp = scratchpad.query_table('searchItems', 'email', '222222')
+    posts = resp.get('Items')
+    print(posts)
     if request.method == "POST":
+        print("userInput() Called!!!!!!!!!!!!!!!!!!!")
         searchword = request.form.get("searchword")
         searchURL = request.form.get("URL")
-
+        response = scratchpad.put_info('222222', searchURL, searchword)
+        print(response)
         print(searchword)
         print(searchURL)
-    return render_template('index.html')
-
+    return render_template('index.html', posts=posts)
 
 
--- scratchpad.py
+++ scratchpad.py
@@ -2,7 +2,10 @@ from boto3 import resource
 from boto3.dynamodb.conditions import Key
 
 # THIS ACTUALLY WORKS!!! Stolen from https://stackoverflow.com/questions/53935211/query-all-items-by-partition-key-in-dynamo-using-boto3
+
+
 dynamodb_resource = resource('dynamodb')
+searchdatabase = dynamodb_resource.Table("searchItems")
 
 def query_table(table_name, key=None, value=None):
     table = dynamodb_resource.Table(table_name)
@@ -15,13 +18,31 @@ def query_table(table_name, key=None, value=None):
 
 
 
+def put_info(email, site, searchterm):
+    response = searchdatabase.put_item(
+            Item={
+                'email': email,
+                'ItemNumber': 12,
+                'siteURL': site,
+                'searchterm': searchterm,
+                })
+    status_code = response['ResponseMetadata']['HTTPStatusCode']
+    print(status_code)
+    print(response) 
+
+
+
 
 if __name__ == '__main__':
-    resp = query_table(
-        table_name='searchItems', 
-        key='email', 
-        value='222222'
-    )
-    items = resp.get('Items')
-    print(len(items))
-    print(items)
+    response = put_info('222222', 'FUCKasdfasdfa', 'YOUasdfasdweqrwe32')
+    print('resp = ', response)
+  
+
+    # resp = query_table(
+    #     table_name='searchItems', 
+    #     key='email', 
+    #     value='222222'
+    # )
+    # items = resp.get('Items')
+    # print(len(items))
+    # print(items)
