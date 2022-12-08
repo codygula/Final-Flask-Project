9d8c803717fe4c9832b86aa58df614a941ce4a6c
Cody Gula <cgula7@gmail.com>
2022-12-07 19:55:39 -0800

    improvements

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import boto3
 import scratchpad 
 import time
+import json
 
 dbclient = boto3.resource("dynamodb")
 searchdatabase = dbclient.Table("searchItems")
@@ -14,16 +15,16 @@ app = Flask(__name__)
 def UserInput():
     resp = scratchpad.query_table('searchItems', 'email', '222222')
     posts = resp.get('Items')
-    print(posts)
-    if request.method == "POST":
-        if request.form.get('Add') == 'Add':
-            print("userInput() Called!!!!!!!!!!!!!!!!!!!")
-            searchword = request.form.get("searchword")
-            searchURL = request.form.get("URL")
-            response = scratchpad.put_info('222222', searchURL, searchword)
-            print(response)
-            print(searchword)
-            print(searchURL)
+    # print(posts)
+    # if request.method == "POST":
+    #     if request.form.get('Add') == 'Add':
+    #         print("userInput() Called!!!!!!!!!!!!!!!!!!!")
+    #         searchword = request.form.get("searchword")
+    #         searchURL = request.form.get("URL")
+    #         response = scratchpad.put_info('222222', searchURL, searchword)
+    #         print(response)
+    #         print(searchword)
+    #         print(searchURL)
     return render_template('index.html', posts=posts)
 
 @app.route('/DeleteItem',  methods =["POST"])
@@ -52,20 +53,24 @@ def TestItem():
     posts = resp.get('Items')
     return render_template('index.html', posts=posts)
 
-@app.route('/AddItem', methods=["POST"])
+@app.route('/AddItem', methods=["GET","POST"])
 def AddItem():
     print("AddItem() Called")
+
     information = request.data
     testThis = (information.decode("utf-8"))
+    data = json.loads(testThis)
+
     print("testThis= ", testThis)
-    print(type(information))
-    print(type(testThis))
-    print("AddItem() information = ", information)
+    print("AddItem() data[item] = ", data["item"])
+    print("AddItem() data[URL] = ", data["URL"])
 
     # searchword = request.form.get("searchword")
     # searchURL = request.form.get("URL")
-    # response = scratchpad.put_info('222222', searchURL, searchword)
-
+    response = scratchpad.put_info('222222', data["URL"], data["item"])
+    print(response)
+    time.sleep(1)
     resp = scratchpad.query_table('searchItems', 'email', '222222')
     posts = resp.get('Items')
+    
     return render_template('index.html', posts=posts)
